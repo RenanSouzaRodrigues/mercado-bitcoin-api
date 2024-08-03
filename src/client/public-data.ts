bf781ea5eee8954dabeed1fe59a579dafc70b6ff
Renan Souza <renan.souza@powerpush.com.br>
2024-08-03 02:16:08 -0300

    added a lot of types and code identention in all the classes

--- src/client/public-data.ts
+++ src/client/public-data.ts
@@ -1,17 +1,17 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
 export class PublicData {
-    constructor(private baseUrl: string) { }
+    constructor(private baseUrl:string) { }
 
     /**
      * Request for asset's withdraw fee (network fee charge)
      * Has a request limit of 1 request per second
      * @param asset Instrument asset in the form BASE(e.g. USDC)
      */
-    public async getFeesFromAssets(asset: string, params?: {network: string}): Promise<MbFeeFromAsset> {
-        const url = `${this.baseUrl}/${asset}/fees`;
+    public async getFeesFromAssets(asset:string, params?:{network?:string}):Promise<MbFeeFromAsset> {
+        const url:string = `${this.baseUrl}/${asset}/fees`;
         const headers = { "Content-Type": "application/json" };
-        const response = await axios.get(url, {headers, params});
+        const response:AxiosResponse = await axios.get(url, {headers, params});
         return <MbFeeFromAsset> response.data;
     }
 
@@ -19,11 +19,12 @@ export class PublicData {
      * Get current orderbook (depth of market) for the instrument.
      * Has a request limit of 1 request per second
      * @param symbol Instrument symbol in the form BASE-QUOTE(e.g. BTC-BRL)
+     * @param params The query params to filter the request (Not Required)
      */
-    public async getOrderBook(symbol: string, params?: {limit: number}): Promise<MbOrderBook> {
-        const url = `${this.baseUrl}/${symbol}/orderbook`;
+    public async getOrderBook(symbol:string, params?:{limit?:number}):Promise<MbOrderBook> {
+        const url:string = `${this.baseUrl}/${symbol}/orderbook`;
         const headers = {"Content-Type": "application/json"};
-        const response = await axios.get(url, {headers, params});
+        const response:AxiosResponse = await axios.get(url, {headers, params});
         return <MbOrderBook> response.data;
     }
 
@@ -31,34 +32,37 @@ export class PublicData {
      * List trades (executions).
      * Has a request limit of 1 request per second
      * @param symbol Instrument symbol in the form BASE-QUOTE(e.g. BTC-BRL)
+     * @param params The query params to filter the request (Not Required)
      */
-    public async listTrades(symbol: string, params?: {tid: number, since: number, from: number, to: number, limit: number}): Promise<MbOrder[]> {
-        const url = `${this.baseUrl}/${symbol}/trades`;
+    public async listTrades(symbol:string, params?:{tid:number, since:number, from:number, to:number, limit:number}):Promise<MbOrder[]> {
+        const url:string = `${this.baseUrl}/${symbol}/trades`;
         const headers = {"Content-Type": "application/json"};
-        const response = await axios.get(url, {headers, params});
+        const response:AxiosResponse = await axios.get(url, {headers, params});
         return <MbOrder[]> response.data;
     }
 
     /**
      * Request for history bars (candles).
      * Has a request limit of 1 request per second
+     * @param params The query params to filter the request (Not Required)
      */
-    public async listCandles(params?: {symbol: string, resolution: string, to: number, from: number, countback: number}): Promise<MbCandle> {
-        const url = `${this.baseUrl}/candles`;
+    public async listCandles(params?:{symbol:string, resolution:string, to:number, from:number, countback:number}):Promise<MbCandle> {
+        const url:string = `${this.baseUrl}/candles`;
         const headers = {"Content-Type": "application/json"};
-        const response = await axios.get(url, {headers, params});
+        const response:AxiosResponse = await axios.get(url, {headers, params});
         return <MbCandle> response.data; 
     }
 
     /**
      * Get a list of all instruments.
      * Has a request limit of 1 request per second
+     * @param params The query params to filter the request (Not Required)
      */
-    public async listSymbols(params?: {symbols: string}): Promise<MbSymbol> {
-        const url = `${this.baseUrl}/symbols`;
+    public async listSymbols(params?:{symbols: string}):Promise<MbSymbol> {
+        const url:string = `${this.baseUrl}/symbols`;
         const headers = {"Content-Type": "application/json"};
-        const response = await axios.get(url, {headers, params});
-        const payload: MbSymbol = <MbSymbol> response.data;
+        const response:AxiosResponse = await axios.get(url, {headers, params});
+        const payload:MbSymbol = <MbSymbol> response.data;
         payload.base_currency = response.data["base-currency"];
         payload.deposit_minimum = response.data["deposit-minumun"];
         payload.exchange_listed = response.data["exchange-listed"];
@@ -74,10 +78,10 @@ export class PublicData {
      * Has a request limit of 1 request per second
      * @param params The required param to send to the api
      */
-    public async listTickers(params: {symbols: string}): Promise<MbCandle> {
-        const url = `${this.baseUrl}/tickers`;
+    public async listTickers(params:{symbols: string}):Promise<MbCandle> {
+        const url:string = `${this.baseUrl}/tickers`;
         const headers = {"Content-Type": "application/json"};
-        const response = await axios.get(url, {headers, params});
+        const response:AxiosResponse = await axios.get(url, {headers, params});
         return <MbCandle> response.data; 
     }
 
@@ -87,64 +91,64 @@ export class PublicData {
      * Has a request limit of 1 request per second
      * @param asset Instrument asset in the form BASE(e.g. BTC)
      */
-    public async listNetworksFromAsset(asset: string): Promise<MbNetworkFromAsset[]> {
-        const url = `${this.baseUrl}/${asset}/networks`;
+    public async listNetworksFromAsset(asset:string):Promise<MbNetworkFromAsset[]> {
+        const url:string = `${this.baseUrl}/${asset}/networks`;
         const headers = {"Content-Type": "application/json"};
-        const response = await axios.get(url, {headers});
+        const response:AxiosResponse = await axios.get(url, {headers});
         return <MbNetworkFromAsset[]> response.data;
     }
 }
 
 export type MbFeeFromAsset = {
-    asset: string;
-    network: string;
-    deposit_minimum: number;
-    deposit_confirmations_required: number;
-    withdraw_minimum: number;
-    withdrawal_fee: number;
+    asset:string;
+    network:string;
+    deposit_minimum:number;
+    deposit_confirmations_required:number;
+    withdraw_minimum:number;
+    withdrawal_fee:number;
 }
 
 export type MbOrderBook = {
-    asks: number[];
-    bids: number[];
-    timestamp: number;
+    asks:number[];
+    bids:number[];
+    timestamp:number;
 }
 
 export type MbOrder = {
-    amount: number;
-    date: number;
-    price: number;
-    tid: number;
-    type: string;
+    amount:number;
+    date:number;
+    price:number;
+    tid:number;
+    type:string;
 }
 
 export type MbCandle = {
-    c: number[];
-    h: number[];
-    l: number[];
-    o: number[];
-    t: number[];
-    v: number[];
+    c:number[];
+    h:number[];
+    l:number[];
+    o:number[];
+    t:number[];
+    v:number[];
 }
 
 export type MbSymbol = {
-    base_currency: string[];
-    currency: string[];
-    deposit_minimum: number[];
-    description: string[];
-    exchange_listed: boolean[];
-    exchange_traded: boolean[];
-    minmovement: number[];
-    pricescale: number[];
-    session_regular: string[];
-    symbol: string[];
-    timezone: string[];
-    type: string[];
-    withdraw_minimum: number[];
-    withdrawal_fee: number[];
+    base_currency:string[];
+    currency:string[];
+    deposit_minimum:number[];
+    description:string[];
+    exchange_listed:boolean[];
+    exchange_traded:boolean[];
+    minmovement:number[];
+    pricescale:number[];
+    session_regular:string[];
+    symbol:string[];
+    timezone:string[];
+    type:string[];
+    withdraw_minimum:number[];
+    withdrawal_fee:number[];
 }
 
 export type MbNetworkFromAsset = {
-    coin: string;
-    network: string;
+    coin:string;
+    network:string;
 }
--- src/client/trading.ts
+++ src/client/trading.ts
@@ -1,13 +1,7 @@
 import axios, { AxiosResponse } from "axios";
 
 export class Trading {
-    private bearerToken: string;
-    private baseUrl: string;
-
-    constructor(bearerToken: string, url: string) {
-        this.bearerToken = `Bearer ${bearerToken}`;
-        this.baseUrl = url + "/account";
-    }
+    constructor(private bearerToken:string, private baseUrl:string) { }
 
     /**
      * List orders from specific market (most recent first)
@@ -16,20 +10,20 @@ export class Trading {
      * @param symbol Instrument symbol in the form BASE-QUOTE(e.g. BTC-BRL)
      * @param params The query params to filter the request (Not Required)
      */
-    public async listOrders(accountId: string, symbol: string, params?: {
-        has_executions?: string,
-        side?: 'buy' | 'sell',
-        status?: 'created' | 'working' | 'cancelled' | 'filled',
-        id_from?: number,
-        id_to?: number,
-        created_at_from?: number,
-        created_at_to?: number,
-        executed_at_from?: number,
-        executed_at_to?: number
-    }): Promise<MbTradingOrder[]> {
-        const url = `${this.baseUrl}/${accountId}/${symbol}/orders`
-        const headers = this.buildHeaders();
-        const response = await axios.get(url, {headers, params});
+    public async listOrders(accountId:string, symbol:string, params?:{
+        has_executions?:string,
+        side?:'buy'|'sell',
+        status?:'created'|'working'|'cancelled'|'filled',
+        id_from?:number,
+        id_to?:number,
+        created_at_from?:number,
+        created_at_to?:number,
+        executed_at_from?:number,
+        executed_at_to?:number
+    }):Promise<MbTradingOrder[]> {
+        const url:string = `${this.baseUrl}/${accountId}/${symbol}/orders`
+        const headers:{'Content-Type':string, 'Authorization':string} = this.buildHeaders();
+        const response:AxiosResponse = await axios.get(url, {headers, params});
         return <MbTradingOrder[]> response.data;
     }
 
@@ -40,10 +34,10 @@ export class Trading {
      * @param symbol Instrument symbol in the form BASE-QUOTE(e.g. BTC-BRL)
      * @param payload The request payload
      */
-    public async placeOrder(accountId: string, symbol: string, payload: MbPlaceOrderPayload): Promise<MbPlaceOrderResponse> {
-        const url = `${this.baseUrl}/${accountId}/${symbol}/orders`
-        const headers = this.buildHeaders();
-        const response = await axios.post(url, payload, {headers});
+    public async placeOrder(accountId: string, symbol: string, payload: MbPlaceOrderPayload):Promise<MbPlaceOrderResponse> {
+        const url:string = `${this.baseUrl}/${accountId}/${symbol}/orders`
+        const headers:{'Content-Type':string, 'Authorization':string} = this.buildHeaders();
+        const response:AxiosResponse = await axios.post(url, payload, {headers});
         return <MbPlaceOrderResponse> response.data;
     } 
 
@@ -54,10 +48,10 @@ export class Trading {
      * @param orderId Unique order identifier
      * @param params The query params to filter the request (Not Required)
      */
-    public async cancelOrder(accountId: string, symbol: string, orderId: string, params?: {async?: boolean}): Promise<MbCancelOrderResponse> {
-        const url = `${this.baseUrl}/${accountId}/${symbol}/orders/${orderId}`;
-        const headers = this.buildHeaders();
-        const response = await axios.delete(url, {headers});
+    public async cancelOrder(accountId:string, symbol:string, orderId:string, params?:{async?:boolean}):Promise<MbCancelOrderResponse> {
+        const url:string = `${this.baseUrl}/${accountId}/${symbol}/orders/${orderId}`;
+        const headers:{'Content-Type':string, 'Authorization':string} = this.buildHeaders();
+        const response:AxiosResponse = await axios.delete(url, {headers});
         return <MbCancelOrderResponse> response.data;
     }
 
@@ -67,9 +61,9 @@ export class Trading {
      * @param symbol Instrument symbol in the form BASE-QUOTE(e.g. BTC-BRL)
      * @param orderId Unique order identifier
      */
-    public async getOrders(accountId: string, symbol: string, orderId: string): Promise<MbTradingOrder> {
-        const url = `${this.baseUrl}/${accountId}/${symbol}/orders/${orderId}`;
-        const headers = this.buildHeaders();
+    public async getOrders(accountId: string, symbol: string, orderId: string):Promise<MbTradingOrder> {
+        const url:string = `${this.baseUrl}/${accountId}/${symbol}/orders/${orderId}`;
+        const headers:{'Content-Type':string, 'Authorization':string} = this.buildHeaders();
         const response = await axios.get(url, {headers});
         return <MbTradingOrder> response.data;
     }
@@ -79,10 +73,10 @@ export class Trading {
      * @param accountId Account identifier. Obtained from List Accounts
      * @param params The query params to filter the request (Not Required)
      */
-    public async cancelAllOpenOrders(accountId: string, params?: {has_executions?: boolean, symbol?: string}): Promise<MbCancelAllOrdersResponse> {
-        const url: string = `${this.baseUrl}/${accountId}/cancel_all_open_orders`
-        const headers = this.buildHeaders();
-        const response = await axios.delete(url, {headers, params});
+    public async cancelAllOpenOrders(accountId:string, params?:{has_executions?:boolean, symbol?:string}):Promise<MbCancelAllOrdersResponse> {
+        const url:string = `${this.baseUrl}/${accountId}/cancel_all_open_orders`
+        const headers:{'Content-Type':string, 'Authorization':string} = this.buildHeaders();
+        const response:AxiosResponse = await axios.delete(url, {headers, params});
         return <MbCancelAllOrdersResponse> response.data;
     }
 
@@ -92,91 +86,88 @@ export class Trading {
      * @param params The query params to filter the request (Not Required)
      * @returns 
      */
-    public async listAllOrders(accountId: string, params?: {has_executions?: boolean, symbol?: string, status?: string, size?: number}): Promise<MbCompletedOrder[]> {
-        const url: string = `${this.baseUrl}/${accountId}/orders`;
-        const headers = this.buildHeaders();
-        const response: AxiosResponse = await axios.get(url, {headers});
+    public async listAllOrders(accountId:string, params?:{has_executions?:boolean, symbol?:string, status?:string, size?:number}):Promise<MbCompletedOrder[]> {
+        const url:string = `${this.baseUrl}/${accountId}/orders`;
+        const headers:{'Content-Type':string, 'Authorization':string} = this.buildHeaders();
+        const response:AxiosResponse = await axios.get(url, {headers});
         return <MbCompletedOrder[]> response.data.items;
     }
 
-    private buildHeaders(): {'Content-Type': string, 'Authorization': string} {
-        return {
-            "Content-Type": "application/json",
-            "Authorization": this.bearerToken,
-        }
+    private buildHeaders():{'Content-Type':string, 'Authorization':string} {
+        return { "Content-Type": "application/json", "Authorization": this.bearerToken }
     }
 }
 
 export type MbTradingExecutionOrder = {
-    executed_at: number;
-    fee_rate: number;
-    id: number;
-    instrument: string;
-    price: number;
-    qty: number;
-    side: string;
-    liquidity: string;
+    executed_at:number;
+    fee_rate:number;
+    id:number;
+    instrument:string;
+    price:number;
+    qty:number;
+    side:string;
+    liquidity:string;
 }
 
 export type MbTradingOrder = {
-    avgPrice: number;
-    cost: number;
-    created_at: number;
-    executions: MbTradingExecutionOrder[];
-    externalId: number;
-    fee: number;
-    filledQty: number;
-    id: string;
-    instrument: string;
-    limitPrice: number;
-    qty: number;
-    side: string;
-    status: string;
-    stopPrice: number;
-    triggerOrderId: number;
-    type: string;
-    updated_at: number;
+    avgPrice:number;
+    cost:number;
+    created_at:number;
+    executions:MbTradingExecutionOrder[];
+    externalId:number;
+    fee:number;
+    filledQty:number;
+    id:string;
+    instrument:string;
+    limitPrice:number;
+    qty:number;
+    side:string;
+    status:string;
+    stopPrice:number;
+    triggerOrderId:number;
+    type:string;
+    updated_at:number;
 }
 
 export type MbPlaceOrderPayload = {
-    async: boolean,
-    cost: number,
-    externalId: number,
-    limitPrice: number,
-    qty: number,
-    side: "buy" | "sell",
-    stopPrice: number,
-    type: "market" | "limit" | "stoplimit" | "post-only"
+    async:boolean,
+    cost:number,
+    externalId:number,
+    limitPrice:number,
+    qty:number,
+    side:"buy"|"sell",
+    stopPrice:number,
+    type:"market"|"limit"|"stoplimit"|"post-only"
 }
 
 export type MbPlaceOrderResponse = {
-    orderId: string;
+    orderId:string;
 }
 
 export type MbCancelOrderResponse = {
-    status: string
+    status:string
 }
 
 export type MbCancelAllOrdersResponse = {
-    crypto: string;
-    fiat: string;
-    order_id: string;
-    order_type: string;
-    side: string;
-    status: string;
+    crypto:string;
+    fiat:string;
+    order_id:string;
+    order_type:string;
+    side:string;
+    status:string;
 }
 
 export type MbCompletedOrder = { 
-    created_at: number,
-    filledQty: number,
-    id: string,
-    instrument: string,
-    limitPrice: number,
-    qty: number,
-    side: string,
-    status: string,
-    stopPrice: number,
-    triggerOrderId: string,
-    type: string,
-    updated_at: number
+    created_at:number,
+    filledQty:number,
+    id:string,
+    instrument:string,
+    limitPrice:number,
+    qty:number,
+    side:string,
+    status:string,
+    stopPrice:number,
+    triggerOrderId:string,
+    type:string,
+    updated_at:number
 }
